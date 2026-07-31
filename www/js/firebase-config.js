e0f0ab8ee89a5f250aa2483b7d96957504b44055
FutboLike <quizbook26@gmail.com>
2026-07-31 11:54:21 -0400

    Update firebase-config.js

--- www/js/firebase-config.js
+++ www/js/firebase-config.js
@@ -1,11 +1,11 @@
 // Reemplaza estos valores con la configuración de tu aplicación web de Firebase.
 export const firebaseConfig = {
-  apiKey: "REEMPLAZAR",
-  authDomain: "REEMPLAZAR.firebaseapp.com",
-  projectId: "REEMPLAZAR",
-  storageBucket: "REEMPLAZAR.firebasestorage.app",
-  messagingSenderId: "REEMPLAZAR",
-  appId: "REEMPLAZAR"
+  apiKey: "AIzaSyAjGta-cC89hujvkq8EgQupIy2ID9BZPL0",
+  authDomain: "futbolike-e581a.firebaseapp.com",
+  projectId: "futbolike-e581a",
+  storageBucket: "futbolike-e581a.firebasestorage.app",
+  messagingSenderId: "363416802560",
+  appId: "1:363416802560:web:9342c947cc2d0669fb4fe5"
 };
 
 // Documento esperado: collection "channels", document "futbolike"
